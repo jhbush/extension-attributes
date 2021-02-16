129964cc7264fc081df87a99f6778213b1a27152
Jason Bush <jhbush@mac.com>
2021-02-15 20:11:33 -0500

    Fixed unnecessary ${} on arithmetic variables

--- Current User Directory Size/current_user_directory_size.sh
+++ Current User Directory Size/current_user_directory_size.sh
@@ -10,7 +10,7 @@ userHomeFolderSize=`du -sk /Users/$lastUser/ | awk '{ print $1 }'`
 microsoftData=`du -sk /Users/$lastUser/Documents/Microsoft\ User\ Data | awk '{ print $1 }'`
  
 # Subtracting out Outlook Database Size
-totalSize=$(($userHomeFolderSize - $microsoftData))
+totalSize=$((userHomeFolderSize - microsoftData))
 
 # Converting from Kilobytes to Gigabytes
 GIGABYTES=$(echo "scale=2;${totalSize}/1024/1024"|bc)
